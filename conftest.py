4e9ec8b818f94598702980053a31221f2b0502a5
Ruslan <ruslankhaliullov@gmail.com>
2020-08-10 20:48:33 +0300

    ex. 25

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from fixture.application import Application
 import json
 import os.path
+from fixture.orm import ORMFixture
 
 
 fixture = None
@@ -45,22 +46,3 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
-
-
-#def pytest_generate_tests(metafunc):
-#    for fixture in metafunc.fixturenames:
-#        if fixture.startswith("data_"):
-#            testdatas = load_from_module(fixture[5:])
-#            metafunc.parametrize(fixture, testdatas, ids=(str(x) for x in testdatas))
-#        elif fixture.startswith("json_"):
-#            testdatas = load_from_json(fixture[5:])
-#            metafunc.parametrize(fixture, testdatas, ids=(str(x) for x in testdatas))
-
-
-#def load_from_module(module):
-#     return importlib.import_module("data.%s" % module).testdatas
-
-
-#def load_from_json(file):
-#    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-#        return jsonpickle.decode(f.read()) 
--- fixture/application.py
+++ fixture/application.py
@@ -10,6 +10,7 @@ import unittest
 
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.project import ProjectHelper
 
 
 class Application:
@@ -24,6 +25,7 @@ class Application:
             raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
+        self.project = ProjectHelper(self)
         self.base_url = base_url
 
     def is_valid(self):
